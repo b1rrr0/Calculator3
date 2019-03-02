d6ca1c5fd57d31f52906ff94f248eab604f69fd2
b1rr0 <vashenkonik@gmail.com>
2019-03-02 15:48:57 +0200

    Last Version

---
+++ src/console/calc.java
@@ -0,0 +1,19 @@
+package console;
+public class calc {
+    public static void main(String[] args) {
+         boolean Schetcik =true;
+        _new._Start();
+            while (Schetcik)
+            {
+               int _int1 = _new.MyReadInt();
+               String sign = _new.MyReadChar();
+               int _int2 = _new.MyReadInt();
+               System.out.println("Ответ на вашу задачу " +Result.Check(_int1, sign, _int2));
+               Schetcik=_new._Finish();
+            }
+                                            }
+                    }
+
+
+
+
